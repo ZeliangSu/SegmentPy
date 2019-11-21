1. fixed? NaN-in-Summary bug of batch normalization

layers.py
@@ -339,9 +339,9 @@ def loss_fn(y_true, output_layer, name='loss_fn'):
         loss_op: (tf.loss?) loss operation, by default of a segmentation problem, it's appropriate to use MSE
     """
     with tf.name_scope(name):
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            loss_op = tf.losses.mean_squared_error(labels=tf.cast(y_true, tf.float32), predictions=output_layer)
+        # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        # with tf.control_dependencies(update_ops):  #note: use tf.get_collection(tf.GraphKeys.UPDATE_OPS) manually is better
+        loss_op = tf.losses.mean_squared_error(labels=tf.cast(y_true, tf.float32), predictions=output_layer)
         return loss_op
 
 

main_train.py
@@ -11,19 +11,19 @@ from util import check_N_mkdir
 
 # params
 hyperparams = {
-    'patch_size': 80,
-    'batch_size': 300,  #Xlearn < 20, Unet < 20 saturate GPU memory
+    'patch_size': 512,
+    'batch_size': 5,  #Xlearn < 20, Unet < 20 saturate GPU memory
     'nb_epoch': 100,
     'nb_batch': None,
     'conv_size': 9,
     'nb_conv': 80,
-    'learning_rate': 1e-3,  #float or np.array of programmed learning rate
-    'dropout': 0.5,
+    'learning_rate': 1e-4,  #float or np.array of programmed learning rate
+    'dropout': 0.1,
     'date': '{}_{}_{}'.format(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day),
     'hour': '{}'.format(datetime.datetime.now().hour),
     'device_option': 'specific_gpu:1',
     'augmentation': True,
-    'activation': 'relu',
+    'activation': 'leaky',
     'save_step': 1000,
     'folder_name': None,
 }
@@ -38,7 +38,7 @@ hyperparams['folder_name'] = './logs/{}_bs{}_ps{}_lr{}_cs{}_nc{}_do{}_act_{}{}_c
     hyperparams['dropout'],
     hyperparams['activation'],
     '_aug_' + str(hyperparams['augmentation']),
-    'LRCS_remove_var_aug',  #note: here put your special comment
+    'xlearn_unet_bridge_BN',  #note: here put your special comment
     hyperparams['hour'],
 )
 
@@ -53,7 +53,7 @@ train_inputs = inputpipeline(hyperparams['batch_size'], suffix='train', augmenta
 test_inputs = inputpipeline(hyperparams['batch_size'], suffix='test')
 
 # init model
-nodes = model_LRCS_custom(train_inputs,
+nodes = model_xlearn_lite(train_inputs,
                    test_inputs,
                    hyperparams['patch_size'],
                    hyperparams['batch_size'],

train.py
@@ -76,19 +76,21 @@ def train(nodes, train_inputs, test_inputs, hyperparams, save_step=200, device_o
                             })
                             cv_writer.add_summary(summary, ep * hyperparams['nb_batch'] + step)
 
+                        if step % 9 == 4: #note: (solution) unknown cross-validation nan-in-summary bug on batch norm
                             # in situ testing without loading weights unlike cs-230-stanford
                             summary, _, _, _ = sess.run([nodes['summary'], nodes['y_pred'], nodes['loss_update_op'], nodes['acc_update_op']],
                                                   feed_dict={nodes['train_or_test']: 'test',
                                                              nodes['drop']: 1,
                                                              nodes['learning_rate']: learning_rate,
-                                                             nodes['BN_phase']: False,
+                                                             nodes['BN_phase']: False, #fixme: if cv and test False successively throw nan-in-summary error
                                                              })
                             test_writer.add_summary(summary, ep * hyperparams['nb_batch'] + step)
 
-
                         else:
-                            summary, _, _, _ = sess.run(
-                                [nodes['grad_sum'], nodes['train_op'], nodes['loss_update_op'], nodes['acc_update_op']],
+                            summary, _, _, _, _ = sess.run([
+                                nodes['summary'], tf.get_collection(tf.GraphKeys.UPDATE_OPS),
+                                nodes['train_op'], nodes['loss_update_op'], nodes['acc_update_op']
+                            ],
                                 feed_dict={nodes['train_or_test']: 'train',
                                            nodes['drop']: hyperparams['dropout'],
                                            nodes['learning_rate']: learning_rate,
@@ -103,8 +105,6 @@ def train(nodes, train_inputs, test_inputs, hyperparams, save_step=200, device_o
                     if step % save_step == 0:
                         _globalStep = step + ep * hyperparams['nb_batch']
                         saver.save(sess, folder + 'ckpt/step{}'.format(_globalStep))
-
-
         except (KeyboardInterrupt, SystemExit):
             saver.save(sess, folder + 'ckpt/step{}'.format(_globalStep))
         saver.save(sess, folder + 'ckpt/step{}'.format(hyperparams['nb_epoch'] * hyperparams['nb_batch']))