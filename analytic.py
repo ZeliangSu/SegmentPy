1. update inference_and_save_partial_res 2. fix one_hot inverse_one_hot 3. correct logits-->softmax-->loss for classification 4. loop the main_train 5. change model 6. update train.py 7. differentiate _resultwriter and _weightwriter since the to-write-dimensions are different

analytic.py
@@ -2,40 +2,47 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import os
-from util import get_all_trainable_variables, check_N_mkdir, print_nodes_name_shape, clean
+from util import get_all_trainable_variables, check_N_mkdir, print_nodes_name_shape, clean, plot_input_logit_label_diff
 from tsne import tsne, compare_tsne_2D, compare_tsne_3D
 from inference import freeze_ckpt_for_inference
 from PIL import Image
 from scipy import interpolate
 from writer import _resultWriter
 import h5py as h5
-from input import _one_hot
+from input import _one_hot, _minmaxscalar, _inverse_one_hot
+from layers import customized_softmax_np
+
+import logging
+import log
+logger = log.setup_custom_logger('root')
+logger.setLevel(logging.DEBUG)
 
 if os.name == 'posix':  #to fix MAC openMP bug
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+
 # Xlearn
 Xlearn_conserve_nodes = [
-    'model/encoder/conv1/leaky',
-    'model/encoder/conv1bis/leaky',
-    'model/encoder/conv2/leaky',
-    'model/encoder/conv2bis/leaky',
-    'model/encoder/conv3/leaky',
-    'model/encoder/conv3bis/leaky',
-    'model/encoder/conv4/leaky',
-    'model/encoder/conv4bis/leaky',
-    'model/encoder/conv4bisbis/leaky',
+    'model/encoder/conv1/relu',
+    'model/encoder/conv1bis/relu',
+    'model/encoder/conv2/relu',
+    'model/encoder/conv2bis/relu',
+    'model/encoder/conv3/relu',
+    'model/encoder/conv3bis/relu',
+    'model/encoder/conv4/relu',
+    'model/encoder/conv4bis/relu',
+    'model/encoder/conv4bisbis/relu',
     'model/dnn/dnn1/relu',
     'model/dnn/dnn2/relu',
     'model/dnn/dnn3/relu',
-    'model/decoder/deconv5/leaky',
-    'model/decoder/deconv5bis/leaky',
-    'model/decoder/deconv6/leaky',
-    'model/decoder/deconv6bis/leaky',
-    'model/decoder/deconv7bis/leaky',
-    'model/decoder/deconv7bis/leaky',
-    'model/decoder/deconv8/leaky',
-    'model/decoder/deconv8bis/leaky',
+    'model/decoder/deconv5/relu',
+    'model/decoder/deconv5bis/relu',
+    'model/decoder/deconv6/relu',
+    'model/decoder/deconv6bis/relu',
+    'model/decoder/deconv7bis/relu',
+    'model/decoder/deconv7bis/relu',
+    'model/decoder/deconv8/relu',
+    'model/decoder/deconv8bis/relu',
     'model/decoder/logits/add',
 ]
 
@@ -68,26 +75,26 @@ Unet_conserve_nodes = [
 
 # LRCS
 LRCS_conserve_nodes = [
-    'LRCS/encoder/conv1/leaky',
-    'LRCS/encoder/conv1bis/leaky',
-    'LRCS/encoder/conv2/leaky',
-    'LRCS/encoder/conv2bis/leaky',
-    'LRCS/encoder/conv3/leaky',
-    'LRCS/encoder/conv3bis/leaky',
-    'LRCS/encoder/conv4/leaky',
-    'LRCS/encoder/conv4bis/leaky',
-    'LRCS/encoder/conv4bisbis/leaky',
-    'LRCS/dnn/dnn1/leaky',
-    'LRCS/dnn/dnn2/leaky',
-    'LRCS/dnn/dnn3/leaky',
-    'LRCS/decoder/deconv5/leaky',
-    'LRCS/decoder/deconv5bis/leaky',
-    'LRCS/decoder/deconv6/leaky',
-    'LRCS/decoder/deconv6bis/leaky',
-    'LRCS/decoder/deconv7/leaky',
-    'LRCS/decoder/deconv7bis/leaky',
-    'LRCS/decoder/deconv8/leaky',
-    'LRCS/decoder/deconv8bis/leaky',
+    'LRCS/encoder/conv1/relu',
+    'LRCS/encoder/conv1bis/relu',
+    'LRCS/encoder/conv2/relu',
+    'LRCS/encoder/conv2bis/relu',
+    'LRCS/encoder/conv3/relu',
+    'LRCS/encoder/conv3bis/relu',
+    'LRCS/encoder/conv4/relu',
+    'LRCS/encoder/conv4bis/relu',
+    'LRCS/encoder/conv4bisbis/relu',
+    'LRCS/dnn/dnn1/relu',
+    'LRCS/dnn/dnn2/relu',
+    'LRCS/dnn/dnn3/relu',
+    'LRCS/decoder/deconv5/relu',
+    'LRCS/decoder/deconv5bis/relu',
+    'LRCS/decoder/deconv6/relu',
+    'LRCS/decoder/deconv6bis/relu',
+    'LRCS/decoder/deconv7/relu',
+    'LRCS/decoder/deconv7bis/relu',
+    'LRCS/decoder/deconv8/relu',
+    'LRCS/decoder/deconv8bis/relu',
     'LRCS/decoder/logits/identity',
 ]
 
@@ -132,7 +139,7 @@ def load_mainGraph(conserve_nodes, path='./dummy/pb/test.pb'):
     return g_main, ops_dict
 
 
-def inference_and_save_partial_res(g_main, ops_dict, conserve_nodes, hyper=None, input_dir=None, rlt_dir=None, ):
+def inference_and_save_partial_res(g_main, ops_dict, conserve_nodes, hyper=None, input_dir=None, rlt_dir=None):
     """
 
     Parameters
@@ -157,30 +164,36 @@ def inference_and_save_partial_res(g_main, ops_dict, conserve_nodes, hyper=None,
             log_device_placement=False,
             )
     with g_main.as_default() as g_main:
+        # init a writer class
+        plt_illd = plot_input_logit_label_diff()
+
         new_input = g_main.get_tensor_by_name('new_input:0')
 
         # write firstly input and output images
-        imgs = [h5.File(input_dir + '{}.h5'.format(i))['X'] for i in range(hyper['batch_size'])]
+        imgs = [_minmaxscalar(h5.File(input_dir + '{}.h5'.format(i))['X']) for i in range(hyper['batch_size'])]
+        plt_illd.add_input(np.asarray(imgs))
         _resultWriter(imgs, 'input', path=rlt_dir)
-        label = [h5.File(input_dir + '{}.h5'.format(i))['y'] for i in range(hyper['batch_size'])]
-        _resultWriter(label, 'label', path=rlt_dir)
+
+        labels = [np.asarray(h5.File(input_dir + '{}.h5'.format(i))['y']) for i in range(hyper['batch_size'])]
+        plt_illd.add_label(np.asarray(labels))
+        _resultWriter(labels, 'label', path=rlt_dir)
+
         img_size = hyper['patch_size']
+
+        # prepare feed_dict
+        feed_dict = {
+            new_input: np.array(imgs).reshape((hyper['batch_size'], img_size, img_size, 1)),
+        }
+        if hyperparams['batch_normalization']:
+            new_BN_phase = g_main.get_tensor_by_name('new_BN:0')
+            feed_dict[new_BN_phase] = False
 
         try:
             dropout_input = g_main.get_tensor_by_name('new_dropout:0')
-            new_BN_phase = g_main.get_tensor_by_name('new_BN:0')
-            feed_dict = {
-                new_input: np.array(imgs).reshape((hyper['batch_size'], img_size, img_size, 1)),
-                dropout_input: 1.0,
-                new_BN_phase: False,
-            }
+            feed_dict[dropout_input] = 1.0
+
         except Exception as e:
             print('Error(message):', e)
-            new_BN_phase = g_main.get_tensor_by_name('new_BN:0')
-            feed_dict = {
-                new_input: np.array(imgs).reshape((hyper['batch_size'], img_size, img_size, 1)),
-                new_BN_phase: False,
-            }
             pass
 
         # run inference
@@ -195,10 +208,12 @@ def inference_and_save_partial_res(g_main, ops_dict, conserve_nodes, hyper=None,
                 try:
                     if tensors.ndim == 4 or 2:
                         if 'logit' in layer_name:
-                            tensors = tensors[0]  # todo: should generalize to batch
+                            tensors = customized_softmax_np(tensors)
+                            tensors = _inverse_one_hot(tensors)  # note: apply the softmax here
+                            plt_illd.add_logit(tensors)
+
                         else:
-                            _tensors = [np.squeeze(tensors[i]) for i in range(tensors.shape[0])]
-                            tensors = _tensors
+                            tensors = [np.squeeze(tensors[i]) for i in range(tensors.shape[0])]
                 except Exception as e:
                     print(e)
                     pass
@@ -207,22 +222,25 @@ def inference_and_save_partial_res(g_main, ops_dict, conserve_nodes, hyper=None,
                 activations.append(tensors)
 
     # calculate diff by numpy
+    # res[-1] final result
     if hyper['mode'] == 'regression':
-        res_diff = np.equal(np.asarray(np.squeeze(res[-1]), dtype=np.int), np.asarray(label))
+        res_diff = np.equal(np.asarray(np.squeeze(res[-1]), dtype=np.int), np.asarray(labels))
         res_diff = np.asarray(res_diff, dtype=np.int)
         # note: save diff of all imgs
+        plt_illd.add_diff(np.asarray(res_diff))
         _resultWriter(np.transpose(res_diff, (1, 2, 0)), 'diff',
                       path=rlt_dir)  # for diff output shape: [batch, w, h, 1]
     else:
         # one-hot the label
-        label = _one_hot(np.asarray(label))
-        logits = np.asarray(res[-1], dtype=np.int)
-        res_diff = np.equal(clean(logits), np.asarray(label)[-1])
-        _resultWriter(res_diff.astype(int), 'diff',
-                      path=rlt_dir)  # for diff output shape: [batch, w, h, 3]
+        labels = np.expand_dims(np.asarray(labels), axis=3)  # list --> array --> (B, H, W, 1)
+        labels = _one_hot(labels)  # (B, H, W, 3)
+        logits = customized_softmax_np(np.asarray(res[-1], dtype=np.int))  # (B, H, W, 3)
+        res_diff = np.equal(_inverse_one_hot(clean(logits)), _inverse_one_hot(np.asarray(labels)))  #(B, H, W)
+        plt_illd.add_diff(res_diff.astype(int))
+        _resultWriter(res_diff.astype(int), 'diff', path=rlt_dir)  # for diff output shape: [batch, w, h, 3]
 
-
-
+    check_N_mkdir(rlt_dir + 'illd/')
+    plt_illd.plot(out_path=rlt_dir + 'illd/illd.tif')
     # return
     return activations
 
@@ -597,7 +615,12 @@ def weights_euclidean_distance(ckpt_dir=None, rlt_dir=None):
     dic_b['total_std'] = l_total_b_std
 
     # create df
-    dfs = {'weight': pd.DataFrame(dic_w), 'bias': pd.DataFrame(dic_b)}
+    try:
+        dfs = {'weight': pd.DataFrame(dic_w), 'bias': pd.DataFrame(dic_b)}
+    except Exception as e:
+        #note: in a BN network, there are less bias
+        logger.info(e)
+        dfs = {'weight': pd.DataFrame(dic_w)}
 
     # write into excel
     with pd.ExcelWriter(rlt_dir + 'euclidean_dist.xlsx', engine='xlsxwriter') as writer:
@@ -698,7 +721,12 @@ def weights_angularity(ckpt_dir=None, rlt_dir=None):
     dic_b['total_std'] = l_total_b_std
 
     # create df
-    dfs = {'weight': pd.DataFrame(dic_w), 'bias': pd.DataFrame(dic_b)}
+    try:
+        dfs = {'weight': pd.DataFrame(dic_w), 'bias': pd.DataFrame(dic_b)}
+    except Exception as e:
+        #note: in a BN network, there are less bias
+        logger.info(e)
+        dfs = {'weight': pd.DataFrame(dic_w)}
 
     # write into excel
     with pd.ExcelWriter(rlt_dir + 'angularity.xlsx', engine='xlsxwriter') as writer:
@@ -731,7 +759,7 @@ def partialRlt_and_diff(paths=None, hyperparams=None, conserve_nodes=None, plt=F
 
     # run nodes and save results
     inference_and_save_partial_res(g_main, ops_dict, conserve_nodes,
-                                   input_dir=paths['data_dir'], rlt_dir=paths['rlt_dir'] + 'step{}/'.format(paths['step']),
+                                   input_dir=paths['data_dir'], rlt_dir=paths['rlt_dir'] + 'p_inference/step{}/'.format(paths['step']),
                                    hyper=hyperparams)
 
     # plt
@@ -741,6 +769,9 @@ def partialRlt_and_diff(paths=None, hyperparams=None, conserve_nodes=None, plt=F
 
 
 if __name__ == '__main__':
+    # disable the GPU if there's a traning
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
     hyperparams = {
         'patch_size': 512,
         'batch_size': 8,
@@ -749,9 +780,10 @@ if __name__ == '__main__':
         'stride': 1,
         'device_option': 'cpu',
         'mode': 'classification',
+        'batch_normalization': False,
     }
     conserve_nodes = conserve_nodes_dict['LRCS']
-    graph_def_dir = './logs/2019_12_13_bs8_ps512_lr1_cs3_nc48_do0.1_act_leaky_aug_True_mdl_LRCS_mode_classification_comment_Cross_entropy_on_hot_1/hour16/'
+    graph_def_dir = './logs/2019_12_18_bs8_ps512_lr1e-05_cs3_nc48_do0.1_act_relu_aug_True_mdl_LRCS_mode_classification_comment_Cross_entropy_correct_the_one_hot_func/hour9/'
     step = 0
     step_init = 0
     paths = {
@@ -769,10 +801,10 @@ if __name__ == '__main__':
         'tsne_path':  graph_def_dir + 'tsne/',
     }
     print('Proceed step {}'.format(paths['step']))
+    # visualize_weights(params=paths)
     partialRlt_and_diff(paths=paths, hyperparams=hyperparams, conserve_nodes=conserve_nodes)
-    visualize_weights(params=paths)
 
-    step = 174964
+    step = 19155
     paths = {
         'step': step,
         'perplexity': 100,  #default 30 usual range 5-50
@@ -789,10 +821,10 @@ if __name__ == '__main__':
         'tsne_path':  graph_def_dir + 'tsne/',
     }
     print('Proceed step {}'.format(paths['step']))
+    # visualize_weights(params=paths)
     partialRlt_and_diff(paths=paths, hyperparams=hyperparams, conserve_nodes=conserve_nodes)
-    tsne_on_weights(params=paths, mode='2D')
-    tsne_on_bias(params=paths, mode='2D')
-    visualize_weights(params=paths)
-    weights_euclidean_distance(ckpt_dir=paths['ckpt_dir'], rlt_dir=paths['rlt_dir'])
-    weights_angularity(ckpt_dir=paths['ckpt_dir'], rlt_dir=paths['rlt_dir'])
+    # tsne_on_weights(params=paths, mode='2D')
+    # tsne_on_bias(params=paths, mode='2D')
+    # weights_euclidean_distance(ckpt_dir=paths['ckpt_dir'], rlt_dir=paths['rlt_dir'])
+    # weights_angularity(ckpt_dir=paths['ckpt_dir'], rlt_dir=paths['rlt_dir'])
     # weights_hists_2excel(ckpt_dir=paths['ckpt_dir'], rlt_dir=paths['rlt_dir'])

inference.py
@@ -6,10 +6,16 @@ from itertools import product
 from PIL import Image
 from proc import _stride
 from tqdm import tqdm
+from input import _minmaxscalar
 from util import print_nodes_name
 from tensorflow.python.tools.optimize_for_inference_lib import optimize_for_inference
 from tensorflow.python.framework import dtypes
 
+# logging
+import logging
+import log
+logger = log.setup_custom_logger(__name__)
+logger.setLevel(logging.INFO)
 
 class reconstructor_V2():
     def __init__(self, image_size, patch_size, stride):
@@ -75,34 +81,39 @@ def reconstruct(stack, image_size=None, stride=None):
 def freeze_ckpt_for_inference(paths=None, hyper=None, conserve_nodes=None):
     assert isinstance(paths, dict), 'The paths parameter expected a dictionnay but other type is provided'
     assert isinstance(hyper, dict), 'The hyper parameter expected a dictionnay but other type is provided'
+    assert isinstance(conserve_nodes, list), 'The name of the conserve node should be in a list'
 
     # clean graph first
     tf.reset_default_graph()
 
     # freeze ckpt then convert to pb
     new_input = tf.placeholder(tf.float32, shape=[None, hyper['patch_size'], hyper['patch_size'], 1], name='new_input')
-    new_BN = tf.placeholder(tf.bool, name='new_BN')
+    new_BN = tf.placeholder_with_default(False, [], name='new_BN')  #note: it seems like T/F after freezing isn't important
 
     # load meta graph
+    input_map = {
+        'input_pipeline_train/IteratorGetNext': new_input,
+    }
     try:
-        new_dropout = tf.placeholder(tf.float32, shape=[], name='new_dropout')
+        new_dropout = tf.placeholder_with_default(1.0, [],
+                                                  name='new_dropout')  # note: use ph_with_default so during inference dont need to call
+        input_map['dropout_prob'] = new_dropout
+        if hyper['batch_normalization']:
+            input_map['BN_phase'] = new_BN
+
         restorer = tf.train.import_meta_graph(
             paths['ckpt_path'] + '.meta',
-            input_map={
-                'input_pipeline_train/IteratorGetNext': new_input,
-                'dropout_prob': new_dropout,
-                'BN_phase': new_BN,
-            },
+            input_map=input_map,
             clear_devices=True,
         )
     except Exception as e:
-        print('Error(msg):', e)
+        if hyper['batch_normalization']:
+            input_map['BN_phase'] = new_BN
+
+        logger.warning('Error(msg):', e)
         restorer = tf.train.import_meta_graph(
             paths['ckpt_path'] + '.meta',
-            input_map={
-                'input_pipeline_train/IteratorGetNext': new_input,
-                'BN_phase': new_BN,
-            },
+            input_map=input_map,
             clear_devices=True,
         )
 
@@ -144,15 +155,6 @@ def optimize_pb_for_inference(paths=None, conserve_nodes=None):
     # clean graph first
     tf.reset_default_graph()
     check_N_mkdir(paths['optimized_pb_dir'])
-    # #fixme: the following won't throw error
-    # os.system(
-    #     "python -m tensorflow.python.tools.optimize_for_inference --input {} --output {} --input_names={} --output_names={}".format(
-    #         paths['save_pb_path'],
-    #         paths['optimized_pb_path'],
-    #         'input_ph,new_BN_phase',#fixme: change here
-    #         conserve_nodes[-1]
-    #     )
-    # )
 
     # load protobuff
     with tf.gfile.FastGFile(paths['save_pb_path'], "rb") as f:
@@ -213,8 +215,8 @@ def inference_recursive(inputs=None, conserve_nodes=None, paths=None, hyper=None
         #todo: replacee X with a inputpipeline
         X = G.get_tensor_by_name('import/' + 'new_input:0')
         y = G.get_tensor_by_name('import/' + conserve_nodes[-1] + ':0')
-        bn = G.get_tensor_by_name('import/' + 'new_BN:0')
-        do = G.get_tensor_by_name('import/' + 'new_dropout:0')
+        # bn = G.get_tensor_by_name('import/' + 'new_BN:0')  #note: not needed anymore
+        # do = G.get_tensor_by_name('import/' + 'new_dropout:0')  #note: not needed anymore
 
         # compute the dimensions of the patches array
         for i, _input in tqdm(enumerate(inputs), desc='image'):
@@ -224,9 +226,9 @@ def inference_recursive(inputs=None, conserve_nodes=None, paths=None, hyper=None
             n_h, n_w = output.get_nb_patch()
             hyper['nb_batch'] = n_h * n_w // hyper['batch_size']
             last_batch_len = n_h * n_w % hyper['batch_size']
-            print('\nnumber of batch: {}'.format(hyper['nb_batch']))
-            print('\nbatch size: {}'.format(hyper['batch_size']))
-            print('\nlast batch size: {}'.format(last_batch_len))
+            logger.info('\nnumber of batch: {}'.format(hyper['nb_batch']))
+            logger.info('\nbatch size: {}'.format(hyper['batch_size']))
+            logger.info('\nlast batch size: {}'.format(last_batch_len))
 
             # inference
             for i_batch in tqdm(range(hyper['nb_batch'] + 1), desc='batch'):
@@ -238,9 +240,9 @@ def inference_recursive(inputs=None, conserve_nodes=None, paths=None, hyper=None
                     for id in np.nditer(id_list):
                         b = id // n_h
                         a = id % n_h
-                        print('\n id: {}'.format(id))
-                        print('\n row coordinations: {} - {}'.format(a * hyper['stride'], a * hyper['stride'] + hyper['patch_size']))
-                        print('\n colomn coordinations: {} - {}'.format(b * hyper['stride'], b * hyper['stride'] + hyper['patch_size']))
+                        logger.debug('\n id: {}'.format(id))
+                        logger.debug('\n row coordinations: {} - {}'.format(a * hyper['stride'], a * hyper['stride'] + hyper['patch_size']))
+                        logger.debug('\n colomn coordinations: {} - {}'.format(b * hyper['stride'], b * hyper['stride'] + hyper['patch_size']))
                         # concat patch to batch
                         batch.append(_input[
                                      a * hyper['stride']: a * hyper['stride'] + hyper['patch_size'],
@@ -248,17 +250,17 @@ def inference_recursive(inputs=None, conserve_nodes=None, paths=None, hyper=None
                                      ])
 
                     # inference
-                    batch = np.asarray(batch)
+                    batch = np.asarray(_minmaxscalar(batch))  #note: don't forget the minmaxscalar, since during training we put it
                     batch = np.expand_dims(batch, axis=3)  # ==> (8, 512, 512, 1)
                     feed_dict = {
                         X: batch,
-                        do: 1.0,
-                        bn: False,
+                        # do: 1.0,  #note: not needed anymore
+                        # bn: False,  #note: not needed anymore
                     }
                     _out = sess.run(y, feed_dict=feed_dict)
                     output.add_batch(np.squeeze(_out), start_id)
                 else:
-                    print('last batch')
+                    logger.info('last batch')
                     start_id = i_batch * hyper['batch_size']
                     batch = []
                     # construct input
@@ -272,7 +274,7 @@ def inference_recursive(inputs=None, conserve_nodes=None, paths=None, hyper=None
                                      ])
 
                     # 0-padding batch
-                    batch = np.asarray(batch)
+                    batch = np.asarray(_minmaxscalar(batch))
                     batch = np.expand_dims(batch, axis=3)
                     batch = np.concatenate([batch, np.zeros(
                         (hyper['batch_size'] - last_batch_len + 1, *batch.shape[1:])
@@ -280,8 +282,8 @@ def inference_recursive(inputs=None, conserve_nodes=None, paths=None, hyper=None
 
                     feed_dict = {
                         X: batch,
-                        do: 1.0,
-                        bn: False,
+                        # do: 1.0,  #note: not needed anymore
+                        # bn: False,  #note: not needed anymore
                     }
                     _out = sess.run(y, feed_dict=feed_dict)
                     output.add_batch(np.squeeze(_out[:last_batch_len]), start_id)

input.py
@@ -8,7 +8,7 @@ from augmentation import random_aug
 import logging
 import log
 logger = log.setup_custom_logger('root')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO )
 
 def inputpipeline(batch_size, ncores=mp.cpu_count(), suffix='', augmentation=False, mode='regression'):
     """
@@ -167,7 +167,7 @@ def parse_h5_one_hot(fname, patch_size):
         y = _one_hot(y)
         logger.debug('y shape: {}, nb_class: {}'.format(y.shape, y.shape[-1]))  #B, H, W, C
 
-        return _minmaxscalar(X), y.astype(np.int64)
+        return _minmaxscalar(X), y.astype(np.int32)
 
 
 def parse_h5(fname, patch_size):
@@ -210,7 +210,7 @@ def _minmaxscalar(ndarray, dtype=np.float32):
 def _one_hot(tensor):
     ''' (batch, H, W) --> one hot to --> (batch, H, W, nb_class)'''
     assert isinstance(tensor, np.ndarray), 'Expect input as a np ndarray'
-    logger.debug('tensor shape:{}'.format(tensor.shape))
+    logger.debug('input tensor shape:{}, unique: {}'.format(tensor.shape, np.unique(tensor)))
     if tensor.ndim == 4:
         #note: (Batch, H, W, C)
         tensor = tensor.astype(np.int32)
@@ -225,7 +225,7 @@ def _one_hot(tensor):
         out = np.concatenate(out, axis=3)
 
     elif tensor.ndim == 3:
-        #note: (H, W, C)
+        #note: (H, W, C) no batch size
         tensor = tensor.astype(np.int32)
         # get how many classes
         nb_classes = len(np.unique(tensor))
@@ -233,23 +233,31 @@ def _one_hot(tensor):
         out = []
         for i in range(nb_classes):
             tmp = np.zeros(tensor.shape)
+            tmp[np.where(tensor == i)] = 1
             out.append(tmp)
         # stack along the last channel
         out = np.concatenate(out, axis=2)
     else:
         logger.warning('Oupss!')
         raise NotImplementedError('Oupss!')
+
+    logger.debug('np.shape(out): {}, unique: {}'.format(np.shape(out), np.unique(out)))
     return out
 
 
 def _inverse_one_hot(tensor):
-    assert tensor.dim == 4, 'Expected a tensor of shape (batch, H, W, class)'
-
-    if tensor.dtype == (np.float or np.float32 or np.float64):
+    assert tensor.ndim == 4, 'Expected a tensor of shape (batch, H, W, class)'
+    if tensor.dtype == np.float or tensor.dtype == np.float32 or tensor.dtype == np.float64:
         # make a vote
-        out = np.argmax(tensor, axis=3)
+        output = np.argmax(tensor, axis=3)
+        output = np.expand_dims(output, axis=3)
 
-    elif tensor.dtype == (np.int or np.int8 or np.int32 or np.int64):
+    elif tensor.dtype == np.int or tensor.dtype == np.int8 or tensor.dtype == np.int32 or tensor.dtype == np.int64:
+        output = np.argmax(tensor, axis=3)
+        output = np.expand_dims(output, axis=3)
+
+    else:
         raise NotImplementedError('Implement a mechanism that if several classes is possible, randomly choose one')
 
-    return out
+    return output.astype(np.int32)
+

layers.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
+import numpy as np
 
+# logging
+import logging
+import log
+logger = log.setup_custom_logger(__name__)
+logger.setLevel(logging.INFO)
 
 def init_weights(shape, name='weights', reuse=False):
     """
@@ -153,32 +159,19 @@ def conv2d_layer(input_layer, shape, stride=1, if_BN=True, is_train=None, activa
         if 'logit' in name:
             b = init_bias([shape[3]], name, reuse=reuse)
             output_activation = tf.identity(output + b, name='identity')
+            return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
         else:
             # Batch normalization
             if if_BN:
                 output = batch_norm(output, is_train=is_train, name=name + '_BN', reuse=reuse)
+                output_activation = _activatioin(output, type=activation)
+                return output_activation, {name + '_W': W, name + '_activation': output_activation}
+
             else:
                 b = init_bias([shape[3]], name, reuse=reuse)
                 output = output + b
-
-            # Activation
-            if activation == 'relu':
-                output_activation = tf.nn.relu(output, name='relu')
-            elif activation == 'sigmoid':
-                output_activation = tf.nn.sigmoid(output, name='sigmoid')
-            elif activation == 'tanh':
-                output_activation = tf.nn.tanh(output, name='tanh')
-            elif activation == 'leaky':
-                output_activation = tf.nn.leaky_relu(output, name='leaky')
-            elif '-leaky' in activation:
-                output_activation = tf.nn.leaky_relu(output, alpha=float(activation.split('-')[0]), name='leaky')
-            else:
-                raise NotImplementedError('Activation function not found!')
-        try:
-            return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
-        except Exception as e:
-            print(e)
-            return output_activation, {name + '_W': W, name + '_activation': output_activation}
+                output_activation = _activatioin(output, type=activation)
+                return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
 
 
 def conv2d_transpose_layer(input_layer, shape, output_shape=None, stride=1, if_BN=True, is_train=None, activation='relu', name='', reuse=False):
@@ -199,7 +192,6 @@ def conv2d_transpose_layer(input_layer, shape, output_shape=None, stride=1, if_B
         shape = [shape[0], shape[1], shape[3], shape[2]]  # switch in/output channels [height, width, output_channels, in_channels]
         W = init_weights(shape, name, reuse=reuse)
 
-
         # get batch_size
         dyn_input_shape = tf.shape(input_layer)
         batch_size = dyn_input_shape[0]
@@ -208,37 +200,22 @@ def conv2d_transpose_layer(input_layer, shape, output_shape=None, stride=1, if_B
         transpose = tf.nn.conv2d_transpose(input_layer, W, output_shape=[batch_size, output_shape[1], output_shape[2], output_shape[3]],
                                            strides=[1, stride, stride, 1], padding='SAME', name='transpose')
 
-
         # add activation function
         if 'logit' in name:
             b = init_bias([shape[2]], name, reuse=reuse)
             output_activation = tf.identity(transpose + b, name='identity')
+            return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
         else:
             # Batch Normalization
             if if_BN:
                 output = batch_norm(transpose, is_train=is_train, name=name + '_BN', reuse=reuse)
+                output_activation = _activatioin(output, type=activation)
+                return output_activation, {name + '_W': W, name + '_activation': output_activation}
             else:
                 b = init_bias([shape[2]], name, reuse=reuse)
                 output = transpose + b
-
-            # activation
-            if activation == 'relu':
-                output_activation = tf.nn.relu(output, name='relu')
-            elif activation == 'sigmoid':
-                    output_activation = tf.nn.sigmoid(output, name='sigmoid')
-            elif activation == 'tanh':
-                output_activation = tf.nn.tanh(output, name='tanh')
-            elif activation == 'leaky':
-                output_activation = tf.nn.leaky_relu(output, name='leaky')
-            elif '-leaky' in activation:
-                output_activation = tf.nn.leaky_relu(output, alpha=float(activation.split('-')[0]), name='leaky')
-            else:
-                raise NotImplementedError('Activation function not found!')
-        try:
-            return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
-        except Exception as e:
-            print(e)
-            return output_activation, {name + '_W': W, name + '_activation': output_activation}
+                output_activation = _activatioin(output, type=activation)
+                return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
 
 
 def normal_full_layer(input_layer, size, if_BN=True, is_train=None, activation='relu', name='', reuse=False):
@@ -260,29 +237,21 @@ def normal_full_layer(input_layer, size, if_BN=True, is_train=None, activation='
         W = init_weights([input_size, size], name, reuse=reuse)
         output = tf.matmul(input_layer, W)
 
-        # BN
-        if if_BN:
-            output = batch_norm(output, is_train=is_train, name=name + '_BN', reuse=reuse)
-        else:
+        if 'logit' in name:
             b = init_bias([size], name, reuse=reuse)
-            output = output + b
-
-        # activation
-        if activation == 'relu':
-            output_activation = tf.nn.relu(output, name='relu')
-        elif activation == 'sigmoid':
-            output_activation = tf.nn.sigmoid(output, name='sigmoid')
-        elif activation == 'tanh':
-            output_activation = tf.nn.tanh(output, name='tanh')
-        elif activation == 'leaky':
-            output_activation = tf.nn.leaky_relu(output, name='leaky')
-        else:
-            raise NotImplementedError('Activation function not found!')
-        try:
+            output_activation = tf.identity(output + b, name='identity')
             return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
-        except Exception as e:
-            print(e)
-            return output_activation, {name + '_W': W, name + '_activation': output_activation}
+        else:
+            # BN
+            if if_BN:
+                output = batch_norm(output, is_train=is_train, name=name + '_BN', reuse=reuse)
+                output_activation = _activatioin(output, type=activation)
+                return output_activation, {name + '_W': W, name + '_activation': output_activation}
+            else:
+                b = init_bias([size], name, reuse=reuse)
+                output = output + b
+                output_activation = _activatioin(output, type=activation)
+                return output_activation, {name + '_W': W, name + '_b': b, name + '_activation': output_activation}
 
 
 def dropout(input_layer, hold_prob, name=''):
@@ -463,10 +432,42 @@ def train_operation(adam, gradients, name='train_op'):
         return adam.apply_gradients(gradients, name='applyGrads')
 
 
-def pixel_wise_softmax(inputs):
-    with tf.name_scope("pixel_wise_softmax"):
-        max_axis = tf.reduce_max(inputs, axis=3, keepdims=True)
-        exponential_map = tf.exp(inputs - max_axis)
-        normalize = tf.reduce_sum(exponential_map, axis=3, keepdims=True)
-        return exponential_map / normalize
+def customized_softmax(inputs):
+    with tf.name_scope("customized_softmax"):
+        # todo: inputs = tf.clip_by_value(inputs, min=1e-10)  # to avoid exploding
+        reduce_max = tf.reduce_max(inputs, axis=3, keepdims=True)  #note: ???
+        # note: keepdims=True, max_axis.shape = (B, H, W, 1)
+        # note: keepdims=False max_axis.shape = (B, H, W)
+        nominator = tf.exp(inputs - reduce_max)  #note: here can avoid the loss becoming too big as the number of pixel increases with the number of class
+                                                 # can be demonstrated easily: sum(log(small proba)_i) = inf
+        # nominator = tf.exp(inputs)  #note: shape = (B, H, W, 3)
+        denominator = tf.reduce_sum(nominator, axis=3, keepdims=True)  #note: shape = (B, H, W, 1)
+        return nominator / denominator
 
+
+def _activatioin(output, type='relu'):
+    if type == 'relu':
+        output_activation = tf.nn.relu(output, name='relu')
+    elif type == 'sigmoid':
+        output_activation = tf.nn.sigmoid(output, name='sigmoid')
+    elif type == 'tanh':
+        output_activation = tf.nn.tanh(output, name='tanh')
+    elif type == 'leaky':
+        output_activation = tf.nn.leaky_relu(output, name='leaky')
+    elif '-leaky' in type:
+        output_activation = tf.nn.leaky_relu(output, alpha=float(type.split('-')[0]), name='leaky')
+    else:
+        raise NotImplementedError('Activation function not found!')
+    return output_activation
+
+
+def customized_softmax_np(inputs):
+    # todo: inputs = tf.clip_by_value(inputs, min=1e-10)  # to avoid exploding
+    reduce_max = np.max(inputs, axis=3, keepdims=True)  #note: ???
+    # note: keepdims=True, max_axis.shape = (B, H, W, 1)
+    # note: keepdims=False max_axis.shape = (B, H, W)
+    nominator = np.exp(inputs - reduce_max)  #note: here can avoid the loss becoming too big as the number of pixel increases with the number of class
+                                             # can be demonstrated easily: sum(log(small proba)_i) = inf
+    # nominator = tf.exp(inputs)  #note: shape = (B, H, W, 3)
+    denominator = np.sum(nominator, axis=3, keepdims=True)  #note: shape = (B, H, W, 1)
+    return nominator / denominator

model.py
@@ -4,15 +4,16 @@ from util import print_nodes_name_shape
 
 
 def regression_nodes(pipeline,
-          placeholders=None,
-          model_name='LRCS',
-          patch_size=512,
-          batch_size=200,
-          conv_size=9,
-          nb_conv=80,
-          activation='relu',
-          is_training=False,
-          ):
+                     placeholders=None,
+                     model_name='LRCS',
+                     patch_size=512,
+                     batch_size=200,
+                     conv_size=9,
+                     nb_conv=80,
+                     activation='relu',
+                     batch_norm=True,
+                     is_training=False,
+                     ):
 
     # todo: correct
     # check entries
@@ -95,13 +96,17 @@ def classification_nodes(pipeline,
                          conv_size=9,
                          nb_conv=80,
                          activation='relu',
+                         batch_norm=True,
+                         loss_option='cross_entropy',
                          is_training=False,
-                         loss_option='cross_entropy'):
+                         ):
 
     # check entries
     assert isinstance(placeholders, list), 'placeholders should be a list.'
     # get placeholder
+
     drop_prob, lr, BN_phase = placeholders
+
 
     # build model
     logits, list_params = model_dict[model_name](pipeline=pipeline,
@@ -111,19 +116,21 @@ def classification_nodes(pipeline,
                                                  nb_conv=nb_conv,
                                                  drop_prob=drop_prob,
                                                  activation=activation,
+                                                 if_BN=batch_norm,
                                                  BN_phase=BN_phase,
                                                  reuse=not is_training,
                                                  mode='classification',
                                                  nb_classes=3,
                                                  )
 
+    # logits shape [B, H, W, nb_class]
     with tf.device('/cpu:0'):
         with tf.name_scope('Loss'):
             if loss_option == 'DSC':
-                softmax = pixel_wise_softmax(logits)
+                softmax = customized_softmax(logits)
                 loss = DSC(pipeline['label'], softmax, name='loss_fn')
             elif loss_option == 'cross_entropy':
-                softmax = pixel_wise_softmax(logits)
+                softmax = customized_softmax(logits)
                 loss = Cross_Entropy(pipeline['label'], softmax, name='CE')
             else:
                 raise NotImplementedError('Cannot find the loss option')
@@ -184,7 +191,8 @@ def model_LRCS(pipeline,
                conv_size,
                nb_conv,
                drop_prob,
-               BN_phase,
+               if_BN=True,
+               BN_phase=None,
                activation='relu',
                reuse=False,
                mode='regression',
@@ -219,70 +227,73 @@ def model_LRCS(pipeline,
     with tf.device('/device:GPU:0' if reuse==False else '/device:GPU:1'):
         with tf.name_scope('LRCS'):
             with tf.name_scope('encoder'):
-                conv1, _ = conv2d_layer(pipeline['img'], shape=[conv_size, conv_size, 1, nb_conv],
+                conv1, _ = conv2d_layer(pipeline['img'], shape=[conv_size, conv_size, 1, nb_conv], if_BN=if_BN,
                                         is_train=BN_phase, activation=activation,
                                         name='conv1', reuse=reuse)  # [height, width, in_channels, output_channels]
-                conv1bis, _ = conv2d_layer(conv1, shape=[conv_size, conv_size, nb_conv, nb_conv],
+                conv1bis, _ = conv2d_layer(conv1, shape=[conv_size, conv_size, nb_conv, nb_conv], if_BN=if_BN,
                                            is_train=BN_phase, activation=activation,
                                            name='conv1bis', reuse=reuse)
                 conv1_pooling, ind1 = max_pool_2by2_with_arg(conv1bis, name='maxp1')
 
-                conv2, _ = conv2d_layer(conv1_pooling, shape=[conv_size, conv_size, nb_conv, nb_conv * 2],
+                conv2, _ = conv2d_layer(conv1_pooling, shape=[conv_size, conv_size, nb_conv, nb_conv * 2], if_BN=if_BN,
                                         is_train=BN_phase, activation=activation, name='conv2', reuse=reuse)
-                conv2bis, _ = conv2d_layer(conv2, shape=[conv_size, conv_size, nb_conv * 2, nb_conv * 2],
+                conv2bis, _ = conv2d_layer(conv2, shape=[conv_size, conv_size, nb_conv * 2, nb_conv * 2], if_BN=if_BN,
                                            is_train=BN_phase, activation=activation, name='conv2bis', reuse=reuse)
                 conv2_pooling, ind2 = max_pool_2by2_with_arg(conv2bis, name='maxp2')
 
                 conv3, _ = conv2d_layer(conv2_pooling, shape=[conv_size, conv_size, nb_conv * 2, nb_conv * 4],
-                                        is_train=BN_phase,
+                                        if_BN=if_BN, is_train=BN_phase,
                                         activation=activation, name='conv3', reuse=reuse)
                 conv3bis, m3b = conv2d_layer(conv3, shape=[conv_size, conv_size, nb_conv * 4, nb_conv * 4],
-                                             is_train=BN_phase, activation=activation, name='conv3bis', reuse=reuse)
+                                             if_BN=if_BN, is_train=BN_phase,
+                                             activation=activation, name='conv3bis', reuse=reuse)
                 conv3_pooling, ind3 = max_pool_2by2_with_arg(conv3bis, name='maxp3')
 
                 conv4, m4 = conv2d_layer(conv3_pooling, shape=[conv_size, conv_size, nb_conv * 4, nb_conv * 8],
-                                         is_train=BN_phase, activation=activation, name='conv4', reuse=reuse)
+                                         if_BN=if_BN, is_train=BN_phase,
+                                         activation=activation, name='conv4', reuse=reuse)
                 conv4bis, m4b = conv2d_layer(conv4, shape=[conv_size, conv_size, nb_conv * 8, nb_conv * 8],
-                                             is_train=BN_phase, activation=activation, name='conv4bis', reuse=reuse)
+                                             if_BN=if_BN, is_train=BN_phase,
+                                             activation=activation, name='conv4bis', reuse=reuse)
                 conv4bisbis, m4bb = conv2d_layer(conv4bis, shape=[conv_size, conv_size, nb_conv * 8, 1],
-                                                 is_train=BN_phase, activation=activation,
-                                                 name='conv4bisbis', reuse=reuse)
+                                                 if_BN=if_BN, is_train=BN_phase,
+                                                 activation=activation, name='conv4bisbis', reuse=reuse)
 
             with tf.name_scope('dnn'):
                 conv4_flat = reshape(conv4bisbis, [-1, patch_size ** 2 // 64], name='flatten')
-                full_layer_1, mf1 = normal_full_layer(conv4_flat, patch_size ** 2 // 128, activation=activation,
-                                                      is_train=BN_phase, name='dnn1', reuse=reuse)
+                full_layer_1, mf1 = normal_full_layer(conv4_flat, patch_size ** 2 // 128, activation=activation,  # OOM: //128 --> //512
+                                                      if_BN=if_BN, is_train=BN_phase, name='dnn1', reuse=reuse)
                 full_dropout1 = dropout(full_layer_1, drop_prob, name='dropout1')
-                full_layer_2, mf2 = normal_full_layer(full_dropout1, patch_size ** 2 // 128, activation=activation,
-                                                      is_train=BN_phase, name='dnn2', reuse=reuse)
+                full_layer_2, mf2 = normal_full_layer(full_dropout1, patch_size ** 2 // 128, activation=activation,  # OOM: //128 --> //512
+                                                      if_BN=if_BN, is_train=BN_phase, name='dnn2', reuse=reuse)
                 full_dropout2 = dropout(full_layer_2, drop_prob, name='dropout2')
-                full_layer_3, mf3 = normal_full_layer(full_dropout2, patch_size ** 2 // 64, activation=activation,
-                                                      is_train=BN_phase, name='dnn3', reuse=reuse)
+                full_layer_3, mf3 = normal_full_layer(full_dropout2, patch_size ** 2 // 64, activation=activation,  # OOM: //64 --> //512
+                                                      if_BN=if_BN, is_train=BN_phase, name='dnn3', reuse=reuse)
                 full_dropout3 = dropout(full_layer_3, drop_prob, name='dropout1')
                 dnn_reshape = reshape(full_dropout3, [-1, patch_size // 8, patch_size // 8, 1], name='reshape')
 
             with tf.name_scope('decoder'):
-                deconv_5, m5 = conv2d_layer(dnn_reshape, [conv_size, conv_size, 1, nb_conv * 8],
+                deconv_5, m5 = conv2d_layer(dnn_reshape, [conv_size, conv_size, 1, nb_conv * 8], if_BN=if_BN,
                                            is_train=BN_phase, activation=activation, name='deconv5', reuse=reuse)  # [height, width, in_channels, output_channels]
-                deconv_5bis, _ = conv2d_layer(deconv_5, [conv_size, conv_size, nb_conv * 8, nb_conv * 4],
+                deconv_5bis, _ = conv2d_layer(deconv_5, [conv_size, conv_size, nb_conv * 8, nb_conv * 4], if_BN=if_BN,
                                               is_train=BN_phase, activation=activation, name='deconv5bis', reuse=reuse)
 
                 up1 = up_2by2_ind(deconv_5bis, ind3, name='up1')
-                deconv_6, _ = conv2d_layer(up1, [conv_size, conv_size, nb_conv * 4, nb_conv * 4],
+                deconv_6, _ = conv2d_layer(up1, [conv_size, conv_size, nb_conv * 4, nb_conv * 4], if_BN=if_BN,
                                            is_train=BN_phase, activation=activation, name='deconv6', reuse=reuse)
-                deconv_6bis, _ = conv2d_layer(deconv_6, [conv_size, conv_size, nb_conv * 4, nb_conv * 2],
+                deconv_6bis, _ = conv2d_layer(deconv_6, [conv_size, conv_size, nb_conv * 4, nb_conv * 2], if_BN=if_BN,
                                               is_train=BN_phase, activation=activation, name='deconv6bis', reuse=reuse)
 
                 up2 = up_2by2_ind(deconv_6bis, ind2, name='up2')
-                deconv_7, _ = conv2d_layer(up2, [conv_size, conv_size, nb_conv * 2, nb_conv * 2],
+                deconv_7, _ = conv2d_layer(up2, [conv_size, conv_size, nb_conv * 2, nb_conv * 2], if_BN=if_BN,
                                            is_train=BN_phase, activation=activation, name='deconv7', reuse=reuse)
-                deconv_7bis, _ = conv2d_layer(deconv_7, [conv_size, conv_size, nb_conv * 2, nb_conv],
+                deconv_7bis, _ = conv2d_layer(deconv_7, [conv_size, conv_size, nb_conv * 2, nb_conv], if_BN=if_BN,
                                               is_train=BN_phase, activation=activation, name='deconv7bis', reuse=reuse)
 
                 up3 = up_2by2_ind(deconv_7bis, ind1, name='up3')
-                deconv_8, _ = conv2d_layer(up3, [conv_size, conv_size, nb_conv, nb_conv],
+                deconv_8, _ = conv2d_layer(up3, [conv_size, conv_size, nb_conv, nb_conv], if_BN=if_BN,
                                            is_train=BN_phase, activation=activation, name='deconv8', reuse=reuse)
-                deconv_8bis, _ = conv2d_layer(deconv_8, [conv_size, conv_size, nb_conv, nb_conv],
+                deconv_8bis, _ = conv2d_layer(deconv_8, [conv_size, conv_size, nb_conv, nb_conv], if_BN=if_BN,
                                               is_train=BN_phase, activation=activation, name='deconv8bis', reuse=reuse)
                 logits, m8bb = conv2d_layer(deconv_8bis,
                                             [conv_size, conv_size, nb_conv, 1 if mode == 'regression' else nb_classes],
@@ -546,7 +557,7 @@ def model_xlearn(pipeline,
                 logits, m8bb = conv2d_transpose_layer(deconv_8bis,
                                             [conv_size, conv_size, nb_conv, 1 if mode == 'regression' else nb_classes],
                                             # fixme: batch_size here might not be automatic while inference
-                                            [batch_size, patch_size, patch_size, 1],
+                                            [batch_size, patch_size, patch_size, 1 if mode == 'regression' else nb_classes],
                                             if_BN=False, is_train=BN_phase,
                                             name='logits', reuse=reuse)
         print_nodes_name_shape(tf.get_default_graph())

train.py
@@ -85,16 +85,21 @@ def train_test(train_nodes, test_nodes, train_inputs, test_inputs, hyperparams):
                     # train
                     try:
                         # save summary
+                        # distinguish if use BN and DO
+                        feed_dict = {
+                            train_nodes['learning_rate']: learning_rate,
+                            train_nodes['drop']: hyperparams['dropout'],
+                        }
+
+                        if hyperparams['batch_normalization']:
+                            feed_dict[train_nodes['BN_phase']] = True
+
                         if step % hyperparams['save_summary_step'] == 0:
                             _, _, _, summary, _ = sess.run([
                                 train_nodes['train_op'], train_nodes['loss_update_op'], train_nodes['acc_update_op'],
                                 train_nodes['summary'], update_ops,
                             ],
-                                feed_dict={
-                                    train_nodes['drop']: hyperparams['dropout'],
-                                    train_nodes['learning_rate']: learning_rate,
-                                    train_nodes['BN_phase']: True,
-                                })
+                                feed_dict=feed_dict)
 
                             train_writer.add_summary(summary, ep * hyperparams['nb_batch'] + step)
                             summary_saved_at.append(step)
@@ -102,11 +107,7 @@ def train_test(train_nodes, test_nodes, train_inputs, test_inputs, hyperparams):
                             _, _, _, _ = sess.run([
                                 train_nodes['train_op'], train_nodes['loss_update_op'], train_nodes['acc_update_op'], update_ops
                             ],
-                                feed_dict={
-                                    train_nodes['drop']: hyperparams['dropout'],
-                                    train_nodes['learning_rate']: learning_rate,
-                                    train_nodes['BN_phase']: True,
-                                })
+                                feed_dict=feed_dict)
 
                     except tf.errors.OutOfRangeError as e:
                         saver.save(sess, folder + 'ckpt/step{}'.format(_globalStep))
@@ -127,6 +128,16 @@ def train_test(train_nodes, test_nodes, train_inputs, test_inputs, hyperparams):
                             #
                             ########################
                             if step != 0:
+                                # change feed dict
+                                feed_dict = {
+                                    train_nodes['learning_rate']: 1.0,
+                                    train_nodes['drop']: 1.0,
+                                }
+
+                                if hyperparams['batch_normalization']:
+                                    feed_dict[train_nodes['BN_phase']] = False
+
+                                # load graph in the second device
                                 loader = tf.train.Saver()
                                 # change
                                 ckpt_saved_path = folder + 'ckpt/step{}'.format(_ep * hyperparams['nb_batch'] + _step)
@@ -139,11 +150,7 @@ def train_test(train_nodes, test_nodes, train_inputs, test_inputs, hyperparams):
                                             test_nodes['loss_update_op'],
                                             test_nodes['acc_update_op']
                                         ],
-                                        feed_dict={
-                                            test_nodes['drop']: 1.0,
-                                            test_nodes['learning_rate']: 0,
-                                            test_nodes['BN_phase']: False,
-                                        }
+                                        feed_dict=feed_dict
                                     )
                                     if i_batch == hyperparams['save_step'] // 10 - 1:
                                         test_writer.add_summary(summary, _ep * hyperparams['nb_batch'] + _step)

writer.py
@@ -174,26 +174,123 @@ def _resultWriter(tensor, layer_name='', path=None):
         for i, elt in enumerate(tensor):
             logger.debug('layer_name: {}'.format(layer_name))
             if (True in np.isnan(elt)) or (True in np.isinf(elt)):
-                elt = clean(elt)
-            try:
-                Image.fromarray(np.array(elt)).save(path + '{}/{}.tif'.format(layer_name, i))
-            except Exception as e:
-                logger.debug(e)
-                logger.warning('Clean function cannot clean the nan value from the layer {}! But code continues!'.format(layer_name))
-                pass
+                try:
+                    elt = clean(elt)
+                except Exception as e:
+                    logger.debug(e)
+                    logger.warning('Clean function cannot clean the nan value from the layer {}! '.format(layer_name))
+                    break
+
+            # scope: conv p_infer: (ps, ps, nc)
+            if elt.ndim == 3:
+                # note: save only the first
+                for j in range(elt.shape[-1]):
+                    Image.fromarray(np.asarray(elt[:, :, j])).save(path + '{}/{}.tif'.format(layer_name, j))
+
+            elif elt.ndim == 2:
+                Image.fromarray(np.asarray(elt)).save(path + '{}/{}.tif'.format(layer_name, i))
+
+            # scope: dnn p_infer: (nb_post neuron)
+            elif elt.ndim == 1:
+                # reshape the 1D array
+                ceil = int(np.ceil(np.sqrt(elt.size)))
+                tmp = np.zeros((ceil ** 2), np.float32).ravel()
+                tmp[:elt.size] = elt
+                tmp = tmp.reshape(ceil, ceil)
+
+                Image.fromarray(tmp).save(path + '{}/dnn.tif'.format(layer_name))
+
     else:
-        #  treat dnn
+        #  treat dnn weights
         if tensor.ndim == 1:
-            Image.fromarray(np.expand_dims(tensor, axis=0)).save(path + '{}/dnn.tif'.format(layer_name))
+            # reshape the 1D array
+            ceil = int(np.ceil(np.sqrt(tensor.size)))
+            tmp = np.zeros((ceil ** 2), np.float32).ravel()
+            tmp[:tensor.size] = tensor
+            tmp = tmp.reshape(ceil, ceil)
 
-        #  for one-hoted tensor
+            Image.fromarray(tmp).save(path + '{}/dnn.tif'.format(layer_name))
+
+        #  scope: not I_1_hoted tensor: (B, H, W, C)
         elif tensor.ndim == 4:
-            stack = _inverse_one_hot(tensor)
-            for i in range(stack.shape[0]):
-                Image.fromarray(stack[i]).save([path + '{}/{}.tif'.format(layer_name, i)])
+            tensor = np.squeeze(tensor.astype(np.float32))
+            if 'diff' in layer_name:
+                for i in range(tensor.shape[0]):
+                    Image.fromarray(tensor[i]).save(path + '{}/{}.tif'.format(layer_name, i))
 
-        #  for cnn
-        else:
+        #  for cnn ndim=3
+        elif tensor.ndim == 3:
             for i in range(tensor.shape[2]):
                 Image.fromarray(np.asarray(tensor[:, :, i], dtype=np.float)).save(path + '{}/{}.tif'.format(layer_name, i))
 
+        else:
+            logger.warn('Not implement writer for this kind of data in layer :{}'.format(layer_name))
+            raise NotImplementedError('Ouppss {}'.format(layer_name))
+
+
+def _weighttWriter(tensor, layer_name='', path=None):
+    '''
+    tensor: images(numpy array or list of image) to save of (Height, Width, nth_Conv)
+    path: path(string)
+    layer_name: name of the layer(string)
+    '''
+    # mkdir
+    check_N_mkdir(path + layer_name)
+
+    # for writting inference partial rlt
+    if isinstance(tensor, list):
+        for i, elt in enumerate(tensor):
+            logger.debug('layer_name: {}'.format(layer_name))
+            if (True in np.isnan(elt)) or (True in np.isinf(elt)):
+                try:
+                    elt = clean(elt)
+                except Exception as e:
+                    logger.debug(e)
+                    logger.warning('Clean function cannot clean the nan value from the layer {}! '.format(layer_name))
+                    break
+
+            # scope: conv p_infer: (ps, ps, nc)
+            if elt.ndim == 3:
+                # note: save only the first
+                for j in range(elt.shape[-1]):
+                    Image.fromarray(np.asarray(elt[:, :, j])).save(path + '{}/{}.tif'.format(layer_name, j))
+
+            elif elt.ndim == 2:
+                Image.fromarray(np.asarray(elt)).save(path + '{}/{}.tif'.format(layer_name, i))
+
+            # scope: dnn p_infer: (nb_post neuron)
+            elif elt.ndim == 1:
+                # reshape the 1D array
+                ceil = int(np.ceil(np.sqrt(elt.size)))
+                tmp = np.zeros((ceil ** 2), np.float32).ravel()
+                tmp[:elt.size] = elt
+                tmp = tmp.reshape(ceil, ceil)
+
+                Image.fromarray(tmp).save(path + '{}/dnn.tif'.format(layer_name))
+
+    else:
+        #  treat dnn weights
+        if tensor.ndim == 1:
+            # reshape the 1D array
+            ceil = int(np.ceil(np.sqrt(tensor.size)))
+            tmp = np.zeros((ceil ** 2), np.float32).ravel()
+            tmp[:tensor.size] = tensor
+            tmp = tmp.reshape(ceil, ceil)
+
+            Image.fromarray(tmp).save(path + '{}/dnn.tif'.format(layer_name))
+
+        #  scope: not I_1_hoted tensor: (B, H, W, C)
+        elif tensor.ndim == 4:
+            tensor = np.squeeze(tensor.astype(np.float32))
+            if 'diff' in layer_name:
+                for i in range(tensor.shape[0]):
+                    Image.fromarray(tensor[i]).save(path + '{}/{}.tif'.format(layer_name, i))
+
+        #  for cnn ndim=3
+        elif tensor.ndim == 3:
+            for i in range(tensor.shape[2]):
+                Image.fromarray(np.asarray(tensor[:, :, i], dtype=np.float)).save(path + '{}/{}.tif'.format(layer_name, i))
+
+        else:
+            logger.warn('Not implement writer for this kind of data in layer :{}'.format(layer_name))
+            raise NotImplementedError('Ouppss {}'.format(layer_name))