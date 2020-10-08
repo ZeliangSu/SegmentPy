1. fix beta or bias gradient plot problem

_taskManager/canvas_logic.py
@@ -199,7 +199,7 @@ class gradient_plot(QWidget):
         super().__init__(parent)
 
         self.w = {}
-        self.b = {}
+        self.betaOrBias = {}
         self.gamma = {}
         self.beta = {}
         self.step = {}
@@ -236,9 +236,9 @@ class gradient_plot(QWidget):
             return
 
         # weight
-        fig_w = self.canvas_w.figure
-        fig_w.clear()
-        w_ax = fig_w.add_subplot(131)
+        fig = self.canvas_w.figure
+        fig.clear()
+        w_ax = fig.add_subplot(131)
         w_ax.set_title('weights', fontsize=20)
         df = pd.DataFrame(self.w, index=self.step)
         w_ax.set_xticklabels([n.replace('summary/', '').replace('_0/grad', '') for n in self.w.keys()],
@@ -246,36 +246,30 @@ class gradient_plot(QWidget):
         w_ax.set_xticks(np.arange(len(self.w.keys())))
         w_ax.tick_params(axis='y', which='major', labelsize=20)
         c = w_ax.pcolor(df, cmap='RdBu')
-        fig_w.colorbar(c)
-        # self.canvas_w.draw()
+        fig.colorbar(c)
 
         # gamma
-        # fig_g = self.canvas_gamma.figure
-        # fig_g.clear()
-        g_ax = fig_w.add_subplot(132)
+        g_ax = fig.add_subplot(132)
         g_ax.set_title('gammas', fontsize=20)
         df2 = pd.DataFrame(self.gamma, index=self.step)
-        g_ax.set_xticklabels([n.replace('summary/', '').replace('_0/grad', '') for n in self.gamma.keys()],
+        g_ax.set_xticklabels([n.replace('summary/', '').replace('_0/grad', '').replace('/batch_norm', '') for n in self.gamma.keys()],
                              minor=False, rotation=90, fontsize=20, ha='left')
         g_ax.set_xticks(np.arange(len(self.gamma.keys())))
         g_ax.tick_params(axis='y', which='major', labelsize=20)
         c2 = g_ax.pcolor(df2, cmap='RdBu')
-        fig_w.colorbar(c2)
-        # self.canvas_gamma.draw()
+        fig.colorbar(c2)
 
         # beta
-        # fig_b = self.canvas_gamma.figure
-        # fig_b.clear()
-        b_ax = fig_w.add_subplot(133)
-        b_ax.set_title('betas', fontsize=20)
-        df3 = pd.DataFrame(self.beta, index=self.step)
-        b_ax.set_xticklabels([n.replace('summary/', '').replace('_0/grad', '') for n in self.w.keys()],
+        b_ax = fig.add_subplot(133)
+        b_ax.set_title('betas or bias', fontsize=20)
+        df3 = pd.DataFrame(self.betaOrBias, index=self.step)
+        b_ax.set_xticklabels([n.replace('summary/', '').replace('_0/grad', '').replace('/batch_norm', '')
+                              for n in self.betaOrBias.keys()],
                              minor=False, rotation=90, fontsize=20, ha='left')
-        b_ax.set_xticks(np.arange(len(self.beta.keys())))
+        b_ax.set_xticks(np.arange(len(self.betaOrBias.keys())))
         b_ax.tick_params(axis='y', which='major', labelsize=20)
-        c3 = g_ax.pcolor(df3, cmap='RdBu',
-                         vmin=df3.min().min(), vmax=df3.max().max())
-        fig_w.colorbar(c3)
-        fig_w.tight_layout()
+        c3 = b_ax.pcolor(df3, vmin=df3.min().min(), vmax=df3.max().max())
+        fig.colorbar(c3)
+        fig.tight_layout()
         self.canvas_w.draw()
 

tensorboard_extractor.py
@@ -47,7 +47,7 @@ def gradient_extractor(event_dir: str, write_rlt=True):
         if 'gamma' in grad:
             gamma.append(np.asarray(get_sum(accumulator, grad)[0]))
             gamman.append(grad)
-        elif 'beta' in grad:
+        elif ('beta' in grad) or ('b_0' in grad):
             beta.append(np.asarray(get_sum(accumulator, grad)[0]))
             betan.append(grad)
         elif 'w_0' in grad:
@@ -75,9 +75,11 @@ def gradient_extractor(event_dir: str, write_rlt=True):
     block_mapping = np.stack(block_mapping).transpose()
     layer_mapping = np.stack(layer_mapping).transpose()
     full_mapping = np.stack(mapping, axis=1)
-    gamma = np.stack(gamma, axis=1)
-    beta = np.stack(beta, axis=1)
-    w = np.stack(w, axis=1)
+
+    # take the absolute values of the gradients
+    gamma = np.abs(np.stack(gamma, axis=1))
+    beta = np.abs(np.stack(np.abs(beta), axis=1))
+    w = np.abs(np.stack(np.abs(w), axis=1))
 
     # fold N times then sum
     N = 50
@@ -110,7 +112,7 @@ def gradient_extractor(event_dir: str, write_rlt=True):
         np.savetxt(event_dir + "grad/w.csv", w, delimiter=",")
 
     _gamma = {}
-    _beta = {}
+    _betaOrBias = {}
     _w = {}
 
     for i, n in enumerate(gamman):
@@ -118,12 +120,12 @@ def gradient_extractor(event_dir: str, write_rlt=True):
         _gamma[n] = gamma[::M, i * N]
 
     for i, n in enumerate(betan):
-        _beta[n] = beta[::M, i * N]
+        _betaOrBias[n] = beta[::M, i * N]
 
     for i, n in enumerate(wn):
         _w[n] = w[::M, i * N]
 
-    return block_mapping, full_mapping, _gamma, _beta, _w, step
+    return block_mapping, full_mapping, _gamma, _betaOrBias, _w, step
 
 
 def get_sum(accumulator, param_name):